52658dc066df636abe1a350e4dad2dbb0b26e196
alexbnewman <alex02new@outlook.com>
2023-05-28 22:27:43 -0400

    Can call encode/decode functions on URLs.

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,22 @@
+require './app/services/urlConverter.rb'
 class UrlsController < ApplicationController
+
+  # TODO: Add comments defining functions? What is this called again?
+
+  # TODO: I don't actually know how safe this is... Maybe okay
+  # cuz I'm only kind of using for testing actions, but not sure.
+  # Also, this is to be able to access these routes with Postman 
+  # so I can test things (gets around
+  # issue of CSRF token -- token that verifies all requests are coming from
+  # my own web app).
+  skip_before_action :verify_authenticity_token, only: [:encode, :decode]
+
   def encode
+    #@short_url = UrlConverter.new(params[:original]).encode
+    @short_url = UrlConverter.encode(params[:original])
     @url = Url.new(url_params)
+    # TODO: Do you only want to render JSON for testing? 
     if @url.save
-      # TODO: Fairly sure this is insufficient encoding.
       render json: { short_url: url_for(@url.short) }, status: :created
     else
       render json: { error: 'Failed to create short URL' }, status: :unprocessable_entity
@@ -20,7 +34,7 @@ class UrlsController < ApplicationController
   private
 
   def url_params
-    params.require(:url).permit(:original, :short)
+    params.require(:url).permit(:original, @short_url)
   end
 
 end
--- app/services/urlConverter.rb
+++ app/services/urlConverter.rb
@@ -1,12 +1,22 @@
 # TODO: fille out this class
-class urlConverter
+class UrlConverter
 
+    #Unnecessary things (p. sure):
+
+    #attr_accessor :url
+
+    #def initialize(url)
+    #    @url = url
+    #end
+
+    # Use "self" so that you can call methods without having
+    # to create an actual instance of the class.
     def self.encode(url)
-
+        return url
     end
 
     def self.decode(url)
-
+        return url
     end
 
     private
