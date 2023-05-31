2d7c0a314466203c0216f85eab58341e3bc2b9a0
alexbnewman <alex02new@outlook.com>
2023-05-31 17:41:48 -0400

    Identified hash to use for encoding

--- app/services/urlConverter.rb
+++ app/services/urlConverter.rb
@@ -1,17 +1,16 @@
-# TODO: fille out this class
+require 'digest'
+
+# Note: Using MD5 to hash (uniqueness) and then encoding in Base-62
+# to make hash the correct size (and allow for greater uniqueness than
+# just truncating the hash)
+
 class UrlConverter
-
-    #Unnecessary things (p. sure):
-
-    #attr_accessor :url
-
-    #def initialize(url)
-    #    @url = url
-    #end
-
     # Use "self" so that you can call methods without having
     # to create an actual instance of the class.
+    # TODO: Do I need to call ".to_s" on "url"?
     def self.encode(url)
+        # Hexdigest so encoding is better.
+        Digest::MD5.hexdigest url
         return url
     end
 
